ba738d919be2e8949318914099a7e6079eeafbc5
chung <94212671+Yi-Chung-Chen@users.noreply.github.com>
2023-10-21 01:51:20 +0800

    Update 796_Rotate_String.py

--- 796_Rotate_String.py
+++ 796_Rotate_String.py
@@ -1,7 +1,7 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if set(s) == set(goal) and len(s) == len(goal): # 測資過濾
-            mem = goal #用mem去存取goal最初的資料
+            mem = goal # 用mem去存取goal最初的資料
             goal = goal[1:]+goal[0] # 按照題目的作法 left shift
             if mem == s: # 先檢查mem是否恰好就是s
                 return True
@@ -10,3 +10,4 @@ class Solution:
                     return True
                 goal = goal[1:]+goal[0]
         return False
+# https://leetcode.com/problems/rotate-string/
